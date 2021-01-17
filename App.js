d4c9d214c368341bbbf18e7369b897b75ea23ff6
soolecki <filipsolecki@gmail.com>
2021-01-17 10:42:22 +0100

    Project files

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Animated, Dimensions, SafeAreaView, Platform } from 'react-native';
+
 
 export default function App() {
+
+  const value = useState(new Animated.ValueXY({ x: 0, y: 0 }))[0]
+  const [shape, setShape] = useState('square')
+  const [color, setColor] = useState('red')
+
+
+  const moveToRight = () => {
+    Animated.timing(value, {
+      toValue: { x: (Dimensions.get('window').width - 100), y: 0 },
+      duration: 1000,
+      useNativeDriver: false
+    }).start(() => moveToDown())
+    setShape('square')
+    setColor('red')
+  }
+
+  const moveToDown = () => {
+    Animated.timing(value, {
+      toValue: { x: (Dimensions.get('window').width - 100), y: (Dimensions.get('window').height - 100) },
+      duration: 1000,
+      useNativeDriver: false
+    }).start(() => moveToLeft())
+    setShape('circle')
+    setColor('yellow')
+  }
+
+  const moveToLeft = () => {
+    Animated.timing(value, {
+      toValue: { x: 0, y: (Dimensions.get('window').height - 100) },
+      duration: 1000,
+      useNativeDriver: false
+    }).start(() => moveToUp())
+    setShape('square')
+    setColor('blue')
+  }
+
+  const moveToUp = () => {
+    Animated.timing(value, {
+      toValue: { x: 0, y: 0 },
+      duration: 1000,
+      useNativeDriver: false
+    }).start(() => moveToRight())
+    setShape('circle')
+    setColor('green')
+  }
+
+  useEffect(() => {
+    moveToRight()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Animated.View style={value.getLayout()}>
+        <View style={{ width: 100, height: 100, backgroundColor: color, borderRadius: shape === 'circle' ? 100 / 2 : 0 }}></View>
+      </Animated.View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? 45 : 0
   },
 });
